27e0a02734169c6bfbdea31e7e12f907d7c2936b
Sylvain Rebaud <sylvain@plutinosoft.com>
2012-01-18 23:57:19 -0800

    no need to try to server no body!

--- Source/Core/PltHttpServer.cpp
+++ Source/Core/PltHttpServer.cpp
@@ -228,6 +228,8 @@ PLT_HttpServer::ServeStream(const NPT_HttpRequest&        request,
                             NPT_InputStreamReference&     body, 
                             const char*                   content_type) 
 {    
+    if (body.IsNull()) return NPT_FAILURE;
+    
     // set date
     NPT_TimeStamp now;
     NPT_System::GetCurrentTimeStamp(now);
