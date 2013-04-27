65d08de64fb1f268c56270bc16a0644b99e3ca9e
Sylvain Rebaud <sylvain@plutinosoft.com>
2013-04-27 14:48:26 -0700

    version

--- Source/Platinum/PltVersion.h
+++ Source/Platinum/PltVersion.h
@@ -38,7 +38,7 @@
 /*----------------------------------------------------------------------
 |   constants
 +---------------------------------------------------------------------*/
-#define PLT_PLATINUM_SDK_VERSION            0x00010406
-#define PLT_PLATINUM_SDK_VERSION_STRING     "1.0.4.6"
+#define PLT_PLATINUM_SDK_VERSION            0x00010407
+#define PLT_PLATINUM_SDK_VERSION_STRING     "1.0.4.7"
 
 #endif // _PLT_VERSION_H_
