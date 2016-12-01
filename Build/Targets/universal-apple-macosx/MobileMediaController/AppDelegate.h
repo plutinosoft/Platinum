43cac5b00f6272a5dc6d372033e96b2e49f46214
wangzhizhou <824219521@qq.com>
2016-12-02 00:00:27 +0800

    add the MobileMediaController target

---
+++ Build/Targets/universal-apple-macosx/MobileMediaController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileMediaController
+//
+//  Created by joker on 2016/12/1.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
