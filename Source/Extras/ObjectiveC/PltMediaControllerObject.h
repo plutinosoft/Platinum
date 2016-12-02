c4d59fea063d3ad3ec9de387093d8201252fb417
wangzhizhou <824219521@qq.com>
2016-12-02 22:59:48 +0800

    add the mediaControllerObject file

---
+++ Source/Extras/ObjectiveC/PltMediaControllerObject.h
@@ -0,0 +1,13 @@
+//
+//  PltMediaControllerObject.h
+//  Platinum
+//
+//  Created by joker on 2016/12/2.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PltMediaControllerObject : NSObject
+
+@end
---
+++ Source/Extras/ObjectiveC/PltMediaControllerObject.mm
@@ -0,0 +1,13 @@
+//
+//  PltMediaControllerObject.m
+//  Platinum
+//
+//  Created by joker on 2016/12/2.
+//
+//
+
+#import "PltMediaControllerObject.h"
+
+@implementation PltMediaControllerObject
+
+@end
