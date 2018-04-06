bd8a3c22617bebc50fa41a6e91c0bfa966bc1726
Sylvain Rebaud <sylvain@plutinosoft.com>
2018-04-05 21:54:31 -0700

    Fix windows (#11)
    * Fix windows vs2010 build
    
    * Update Neptune

--- Source/Core/PltDeviceHost.cpp
+++ Source/Core/PltDeviceHost.cpp
@@ -680,7 +680,7 @@ PLT_DeviceHost::ProcessHttpSubscriberRequest(NPT_HttpRequest&              reque
             }
           
             // default lease
-            NPT_Int32 timeout = *PLT_Constants::GetInstance().GetDefaultSubscribeLease().AsPointer();
+            NPT_Int32 timeout = (NPT_Int32)*PLT_Constants::GetInstance().GetDefaultSubscribeLease().AsPointer();
 
             // subscription renewed
             // send the info to the service
@@ -703,7 +703,7 @@ PLT_DeviceHost::ProcessHttpSubscriberRequest(NPT_HttpRequest&              reque
             }
 
             // default lease time
-            NPT_Int32 timeout = *PLT_Constants::GetInstance().GetDefaultSubscribeLease().AsPointer();
+            NPT_Int32 timeout = (NPT_Int32)*PLT_Constants::GetInstance().GetDefaultSubscribeLease().AsPointer();
 
             // send the info to the service
             service->ProcessNewSubscription(m_TaskManager,
