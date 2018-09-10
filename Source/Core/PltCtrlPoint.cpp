ce3e3b92146eec88df2e9f50c8fc40f4e6675c7c
Nick Hingston <nick@wearemothership.com>
2018-09-10 11:50:00 +0100

    APP-1845 fix crash in platinum when InspectDevice called before properly started

--- Source/Core/PltCtrlPoint.cpp
+++ Source/Core/PltCtrlPoint.cpp
@@ -1242,6 +1242,12 @@ PLT_CtrlPoint::InspectDevice(const NPT_HttpUrl& location,
                              NPT_TimeInterval   leasetime)
 {
     NPT_AutoLock lock(m_Lock);
+	
+	if (!m_Started) {
+		NPT_LOG_INFO_1("InspectDevice called before control point started.  location url: %s",
+					   (const char*) location.ToString());
+		return NPT_FAILURE;
+	}
 
     // check if already inspecting device
     NPT_String pending_uuid;
